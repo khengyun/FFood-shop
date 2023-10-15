8e9f31b7f630cb24a17159716c6f5090591461d5
Tiến Thành Hứa <88323009+tien-thanh-hua@users.noreply.github.com>
2023-10-15 13:57:20 +0700

    refactor: remove commented-out queries, update comments for new DB

--- db_scripts/ffood_new_add_data.sql
+++ db_scripts/ffood_new_add_data.sql
@@ -8,10 +8,8 @@ insert into [Admin] (admin_fullname) values (N'Hứa Tiến Thành');
 insert into [Admin] (admin_fullname) values (N'Nguyễn Quốc Anh');
 insert into [Admin] (admin_fullname) values (N'Huỳnh Duy Khang');
 
--- Admin passwords = admin# with # range from 1 to 5
--- For example: thanhhtce171454@fpt.edu.vn: password = admin1
---insert into Account (admin_id, account_username, account_email, account_password, account_type) values (1, N'vuhuynh123', N'vuhuynhce123@fpt.edu.vn', HashBytes('MD5', '12345'), 'admin');
-
+-- Admin passwords = 123456
+-- Admin account ID starts from 1-20
 INSERT INTO Account (admin_id, account_username, account_email, account_password, account_type) VALUES (1, N'vuhuynh123', N'huynhnvnce170550@fpt.edu.vn', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'admin');
 insert into Account (admin_id, account_username, account_email, account_password, account_type) values (2, N'hoangkhang123', N'khangnhce171197@fpt.edu.vn', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'admin');
 insert into Account (admin_id, account_username, account_email, account_password, account_type) values (3, N'khachuy123', N'huyhkce171229@fpt.edu.vn', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'admin');
@@ -19,23 +17,21 @@ insert into Account (admin_id, account_username, account_email, account_password
 insert into Account (admin_id, account_username, account_email, account_password, account_type) values (5, N'quocanh1130', N'anhnqce170483@fpt.edu.vn', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'admin');
 insert into Account (admin_id, account_username, account_email, account_password, account_type) values (6, N'duykhang123', N'khanghdse172647@fpt.edu.vn', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'admin');
 
--- Insert test staff account
+-- Staffs must be added before an associated Account (if exists) can be created
 insert into Staff (staff_fullname) values ('Nguyễn Văn TestStaff');
+-- Staffs' account ID starts from 21-40
 dbcc checkident (Account, RESEED, 20);
 insert into Account (staff_id, account_username, account_email, account_password, account_type) values (1, N'testStaff', N'teststaff@fpt.edu.vn', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'staff');
 
+-- Promotion managers must be added before an associated Account (if exists) can be created
 insert into PromotionManager (pro_fullname) values ('Nguyễn Văn TestPromotion');
+-- Promotion managers' account ID starts from 41-50
 dbcc checkident (Account, RESEED, 40);
 insert into Account(pro_id, account_username, account_email, account_password, account_type) values (1, N'testPromotion', N'testPromotion@fpt.edu.vn', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'promotionManager');
 
--- Customer must be added before an associated Account (if there is one) can be created
+-- Customer must be added before an associated Account (if exists) can be created
 insert into Customer (customer_firstname, customer_lastname, customer_gender, customer_phone, customer_address) values (N'Quoc Anh', N'Nguyen', N'Nam', '0914875606', N'Đường sô 3, Khu Vực Bình thường B, Bình Thủy, Cần Thơ');
-
-
--- Account id 1-20 is reserved for admin accounts
--- User accound id starts from 21
--- TestUser password = testuser
--- Hứa Tiến Thành password = user1234
+-- User account ID starts from 21
 dbcc checkident (Account, RESEED, 50);
 insert into Account (customer_id, account_username, account_email, account_password, account_type) values (1, N'quocanh123', N'anhnq1130@gmail.com', CONVERT(NVARCHAR(32), HashBytes('MD5', '123456'), 2), 'user');
 
@@ -69,11 +65,6 @@ values (2, N'Mì tương đen', N'Mì Tương Đen là một món ăn phổ bi�
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (2, N'Mì quảng', N'Mì Quảng là một món ăn truyền thống của người Việt Nam, với hương vị đặc trưng và đa dạng. Mì mềm thơm, kết hợp với các loại thịt như thịt bò, thịt heo, tôm, mực và các loại rau sống giòn ngon, tạo nên một hương vị độc đáo và hấp dẫn.', 40000, 1, 0, 'https://drive.google.com/uc?id=1gB9FPTZ2hurV4cSZDEvpeQTffGFdGz4g');
 
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (2, N'Mì xào hải sản', 60000, 15, 'https://drive.google.com/uc?id=1b2lndY7jUDPgwV1dMXpx9hAVQZQebvqS');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (2, N'Mì xào bò', 55000, 0, 'https://drive.google.com/uc?id=16blYSeX0hkvED0Fb1dBdSKIVwRzB6_dN');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (2, N'Mì tương đen', 45000, 0, 'https://drive.google.com/uc?id=1u87tcdwWV0-5sYn6RPN208VTZv56UB3u');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (2, N'Mì quảng', 40000, 0, 'https://drive.google.com/uc?id=1gB9FPTZ2hurV4cSZDEvpeQTffGFdGz4g');
-
 -- Banh mi
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (3, N'Bánh mì chả cá', N'Bánh Mì Chả Cá là một món ăn đường phố phổ biến tại Việt Nam. Bánh mì mềm mại và giòn tan được chế biến từ chả cá tươi ngon, kết hợp với các loại rau sống và nước mắc khén, tạo nên hương vị độc đáo và hấp dẫn.', 20000, 1, 0, 'https://drive.google.com/uc?id=1KVJXVXir51PjPQu9ZQlyuiQbQrlwKS5D');
@@ -83,12 +74,6 @@ insert into Food (food_type_id, food_name, food_description, food_price, food_st
 values (3, N'Bánh mì heo quay', N'Bánh Mì Heo Quay là một món ăn ngon và phổ biến tại Việt Nam. Bánh mì giòn tan được kết hợp với lớp thịt heo quay giòn và thơm ngon, kèm theo các loại rau sống và nước sốt mắc khén, tạo nên một trải nghiệm ẩm thực tuyệt vời.', 25000, 1, 0, 'https://drive.google.com/uc?id=1Bd-goOKWwQNjc6M3e6vliZnFSHV4uwlQ');
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (3, N'Bánh mì xíu mại', N'Bánh Mì Xíu Mại là một món ăn đường phố hấp dẫn của Việt Nam. Bánh mì mềm mại chứa bên trong những viên xíu mại thơm ngon, kết hợp với các loại rau sống và nước sốt, tạo nên một hương vị truyền thống và thú vị.', 35000, 1, 50, 'https://drive.google.com/uc?id=1Fgt8Gv9xFnSYl1m-EYgzqj7JPvqM9Tda');
-
-
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (3, N'Bánh mì chả cá', 20000, 0, 'https://drive.google.com/uc?id=1KVJXVXir51PjPQu9ZQlyuiQbQrlwKS5D');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (3, N'Bánh mì thịt nguội', 18000, 0, 'https://drive.google.com/uc?id=1RKM-vmALRleJKjg6w2CPNLearOgFdxyy');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (3, N'Bánh mì heo quay', 25000, 0, 'https://drive.google.com/uc?id=1Bd-goOKWwQNjc6M3e6vliZnFSHV4uwlQ');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (3, N'Bánh mì xíu mại', 35000, 50, 'https://drive.google.com/uc?id=1Fgt8Gv9xFnSYl1m-EYgzqj7JPvqM9Tda');
 
 -- Junk food
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
@@ -100,11 +85,6 @@ values (4, N'Gà xiên que', N'Gà Xiên Que là một món ăn đường phố 
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (4, N'Xúc xích xiên que', N'Xúc Xích Xiên Que là một món ăn độc đáo và ngon miệng. Xúc xích được xiên lên que và chiên giòn. Món ăn thường được ăn kèm với các loại sốt gia vị và rau sống.', 15000, 1, 0, 'https://drive.google.com/uc?id=1jzKat34QrD7PtT-YsgeDYW5fpca8Xo34');
 
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (4, N'Khoai tây chiên', 15000, 0, 'https://drive.google.com/uc?id=1xpFNpt7GRnW9-Z7i0oMJiHtP1_pXEe04');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (4, N'Mực chiên xù', 20000, 0, 'https://drive.google.com/uc?id=1bH_-gCL1S4WZxQ_VjZZmtXBecUiVEqVb');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (4, N'Gà xiên que', 18000, 0, 'https://drive.google.com/uc?id=1Ty6y9P00G49E_zjsZC_rNaA_8uKUFoav');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (4, N'Xúc xích xiên que', 15000, 0, 'https://drive.google.com/uc?id=1jzKat34QrD7PtT-YsgeDYW5fpca8Xo34');
-
 -- Desserts
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (5, N'Bánh flan', N'Bánh Flan là một món tráng miệng ngon miệng được làm từ trứng, đường và sữa. Món ăn có vị ngọt, mềm mịn và thường được phục vụ với caramel đặc biệt ở phía trên.', 10000, 1, 0, 'https://drive.google.com/uc?id=1d4TVY2jIAtI8PWcu06_FtkP2YitNW0DD');
@@ -115,11 +95,6 @@ values (5, N'Sữa chua dẻo', N'Sữa Chua Dẻo là một món tráng miệng
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (5, N'Chè sen nhãn dừa', N'Chè Sen Nhãn Dừa là một món tráng miệng truyền thống của ẩm thực Việt Nam. Món ăn này bao gồm sen, nhãn và dừa tươi ngon, tạo nên hương vị ngọt ngào và mát lạnh. Món ăn thường được phục vụ với đá và thạch dừa.', 18000, 1, 10, 'https://drive.google.com/uc?id=1q7taf4uwKWJLrwttYUBeSuZLk98ytetm');
 
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (5, N'Bánh flan', 10000, 0, 'https://drive.google.com/uc?id=1d4TVY2jIAtI8PWcu06_FtkP2YitNW0DD');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (5, N'Panna cotta', 15000, 0, 'https://drive.google.com/uc?id=1Is76NMoBErXptJ3dwYK-cKgDgnL0Mwki');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (5, N'Sữa chua dẻo', 15000, 0, 'https://drive.google.com/uc?id=1ivbEwy1KgLkDV3NlUtALGU6vN0SQHEEG');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (5, N'Chè sen nhãn dừa', 18000, 10, 'https://drive.google.com/uc?id=1q7taf4uwKWJLrwttYUBeSuZLk98ytetm');
-
 -- Drinks
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (6, N'Coca-Cola (330ml)', N'Coca-Cola là một loại nước ngọt có ga phổ biến trên toàn thế giới. Với hương vị đặc trưng, ngọt ngào và sảy ngon, Coca-Cola thường được phục vụ lạnh và kèm theo đá.', 12000, 1, 0, 'https://drive.google.com/uc?id=14_GTRoOdBs5zuU4VwnzftPsqtdEeW1xP');
@@ -129,11 +104,6 @@ insert into Food (food_type_id, food_name, food_description, food_price, food_st
 values (6, N'Trà đào', N'Trà Đào là một loại trà trái cây ngon mát, được làm từ trà đen pha chế cùng với hương vị tự nhiên và ngọt ngọt của đào. Một lựa chọn tuyệt vời để giải nhiệt và thư giãn trong ngày nắng nóng.', 20000, 1, 0, 'https://drive.google.com/uc?id=1RgBDS_wk1SmUfxMujGQSYMl-uOy8qob3');
 insert into Food (food_type_id, food_name, food_description, food_price, food_status, discount_percent, food_img_url) 
 values (6, N'Trà chanh dây', N'Trà Chanh Dây là một loại trà trái cây tươi ngon, được làm từ trà xanh pha chế cùng với hương vị chua chua ngọt ngọt của chanh dây. Một lựa chọn sức khỏe và thưởng thức tuyệt vời cho mọi ngày.', 20000, 1, 0, 'https://drive.google.com/uc?id=1Cj22ZH1a79m9zz4oKmB-ZhMZuxd24qCx');
-
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (6, N'Coca-Cola (330ml)', 12000, 0, 'https://drive.google.com/uc?id=14_GTRoOdBs5zuU4VwnzftPsqtdEeW1xP');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (6, N'7 Up (330ml)', 12000, 0, 'https://drive.google.com/uc?id=1HxLmW1x2aNiZBmKYGps8hXksinGIl7rv');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (6, N'Trà đào', 20000, 0, 'https://drive.google.com/uc?id=1RgBDS_wk1SmUfxMujGQSYMl-uOy8qob3');
---insert into Food (food_type_id, food_name, food_price, discount_percent, food_img_url) values (6, N'Trà chanh dây', 20000, 0, 'https://drive.google.com/uc?id=1Cj22ZH1a79m9zz4oKmB-ZhMZuxd24qCx');
 
 -- Payment methods
 insert into PaymentMethod (payment_method) values (N'Thẻ tín dụng');
@@ -147,7 +117,7 @@ insert into OrderStatus (order_status) values (N'Đang giao');
 insert into OrderStatus (order_status) values (N'Đã giao');
 insert into OrderStatus (order_status) values (N'Đã hủy');
 
--- Cart, CartItem, Order
+-- Cart, CartItem, Order test data
 insert into Cart (customer_id) values (1);
 
 insert into CartItem (cart_id, food_id, food_price, food_quantity) values (1, 2, 55000, 2);
