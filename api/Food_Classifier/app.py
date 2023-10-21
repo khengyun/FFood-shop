99f17710fb1966c9c53992226261366e8a610aef
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-10-22 06:01:40 +0700

    Update app.py for main mode food classification

--- api/Food_Classifier/app.py
+++ api/Food_Classifier/app.py
@@ -1,116 +1,48 @@
-import uvicorn
-import numpy as np
-import logging
-import PIL
-import PIL.Image
-import pathlib
-import requests
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from sklearn.metrics import *
 from fastapi import FastAPI
-from io import BytesIO
-from fastapi.responses import HTMLResponse
-from fastapi import FastAPI
+from src.predict import predict_dish_onnx
 from starlette.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+import onnxruntime
+import uvicorn
+import time
 
 app = FastAPI()
 
-
-class PredictionRequest(BaseModel):
-    path: str
-    url: str
-
-
-# Load model
-loaded_model = tf.keras.models.load_model('my_model.h5')
-
-
-def predict_img(request: PredictionRequest):
-    """
-    This Function predicts image from file path or url
-
-    Args:
-        request: A PredictionRequest object containing the file path or url of the image to predict.
-
-    Returns:
-        A string describing the image.
-    """
-
-    try:
-        if request.path:
-            img_path = pathlib.Path(request.path)
-            img = image.load_img(img_path, target_size=(160, 160))
-        elif request.url:
-            response = requests.get(request.url)
-            img = PIL.Image.open(BytesIO(response.content))
-            img = image.resize((160, 160), resample=PIL.Image.Resampling.BICUBIC)
-        else:
-            raise Exception('Invalid request: Either path or url must be provided.')
-
-        img_array = image.img_to_array(img)
-        img_batch = np.expand_dims(img_array, axis=0)
-        predictions = loaded_model.predict_on_batch(img_batch).flatten()
-        predictions = tf.nn.sigmoid(predictions)
-        predictions = tf.where(predictions < 0.5, 0, 1)
-        labels = ['Defective Box', 'Non Defective Box']
-        return {'prediction': 'This is a {}'.format(labels[predictions.numpy()[0]])}
-    except Exception as ex:
-        logging.info("Error:", ex)
-        exit('Error Occured: Check File Path or URL')
-
-
-# Đăng ký thư mục chứa tấm ảnh
+# mount data
 app.mount("/images", StaticFiles(directory="images"), name="images")
+app.mount("/model", StaticFiles(directory="model"), name="model")
+app.mount("/src", StaticFiles(directory="src"), name="src")
 
+# mount && load model
+session_onnx = onnxruntime.InferenceSession("model/food_model.onnx", providers=['CPUExecutionProvider'])
 
-@app.get('/')
-def index():
-    message = 'This FFood Image Classification API!'
-    image_filename = 'ok.jpg'  # Tên tập tin ảnh trong thư mục
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return """
+    <html>
+        <head>
+            <title>Welcome to the FFood-shop API Server!</title>
+        </head>
+        <body>
+            <h1>Welcome to the FFood-shop API Server!</h1>
+            <p>Repository: <a href="https://github.com/khengyun/FFood-shop">https://github.com/khengyun/FFood-shop</a></p>
+        </body>
+    </html>
+    """
 
-    # Tạo một chuỗi HTML chứa thẻ <img> với đường dẫn tới tập tin ảnh trong thư mục
-    html_content = f'<html><body><p>{message}</p><img style="width:500px;" src="/images/{image_filename}" alt="Image"></body></html>'
-
+@app.get("/predict_dish_test")
+async def predict_dish(url: str):
+    class_list_file = "model/food_model_labels.txt"
+    
+    start_time = time.time()
+    predicted_dish, img_url = predict_dish_onnx(url, session_onnx, class_list_file)
+    end_time = time.time()
+    
+    response_time = end_time - start_time
+    
+    html_content = f'<html><body><h2>Pred: {predicted_dish}</h2><p>Inference time: {response_time} seconds</p><img style="width:500px;" src="{img_url}" alt="Image"></body></html>'
+    
     return HTMLResponse(content=html_content)
 
-
-@app.get('/predict', response_model=dict)
-def predict(path: str = None, url: str = None):
-    """
-    This Function predicts an image from a file path or URL.
-
-    Args:
-        path: The file path of the image to predict.
-        url: The URL of the image to predict.
-
-    Returns:
-        A dictionary containing the prediction result.
-    """
-    try:
-        if path:
-            img_path = pathlib.Path(path)
-            img = image.load_img(img_path, target_size=(160, 160))
-        elif url:
-            response = requests.get(url)
-            img = PIL.Image.open(BytesIO(response.content))
-            img = img.resize((160, 160), resample=PIL.Image.BICUBIC)
-        else:
-            return {'error': 'Invalid request: Either path or url must be provided.'}
-
-        img_array = image.img_to_array(img)
-        img_batch = np.expand_dims(img_array, axis=0)
-        predictions = loaded_model.predict_on_batch(img_batch).flatten()
-        predictions = tf.nn.sigmoid(predictions)
-        predictions = tf.where(predictions < 0.2, 0, 1)
-        labels = ['Defective Box', 'Non Defective Box']
-        return {'prediction': 'This is a {}'.format(labels[predictions.numpy()[0]])}
-    except Exception as ex:
-        logging.error("Error:", ex)
-        return {'error': 'An error occurred. Check the file path or URL.'}
-
 if __name__ == '__main__':
-    uvicorn.run('app:app', port=8000,host= '0.0.0.0', reload=True)
+    uvicorn.run('app:app', port=8000, host='0.0.0.0', reload=True)
