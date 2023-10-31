52b378402896e0a624408ff9ae3d9e529024bdbf
QuocAnh <kaisoul224@gmail.com>
2023-11-01 03:24:55 +0700

    [update] increace time appear notify

--- src/main/webapp/assets/js/promotionManagerNotify.js
+++ src/main/webapp/assets/js/promotionManagerNotify.js
@@ -5,28 +5,28 @@ $(document).ready(function () {
     $('#success_add_voucher').modal('show');
     setTimeout(function () {
       $('#success_add_voucher').modal('hide');
-    }, 2000);
+    }, 3000);
   };
   
   if (window.location.hash === '#success_update_voucher') {
     $('#success_update_voucher').modal('show');
     setTimeout(function () {
       $('#success_update_voucher').modal('hide');
-    }, 2000);
+    }, 3000);
   };
   
   if (window.location.hash === '#success_update_food') {
     $('#success_update_food').modal('show');
     setTimeout(function () {
       $('#success_update_food').modal('hide');
-    }, 2000);
+    }, 3000);
   };
   
   if (window.location.hash === '#success_delete_voucher') {
     $('#success_delete_voucher').modal('show');
     setTimeout(function () {
       $('#success_delete_voucher').modal('hide');
-    }, 2000);
+    }, 3000);
   };
 });
 
@@ -38,30 +38,30 @@ $(document).ready(function () {
     $('#failure_add_voucher').modal('show');
     setTimeout(function () {
       $('#failure_add_voucher').modal('hide');
-    }, 2000);
+    }, 3000);
   };
   if (window.location.hash === '#failure_add_voucher_exist') {
     $('#failure_add_voucher_exist').modal('show');
     setTimeout(function () {
       $('#failure_add_voucher_exist').modal('hide');
-    }, 2000);
+    }, 3000);
   };
   if (window.location.hash === '#failure_update_voucher') {
     $('#failure_update_voucher').modal('show');
     setTimeout(function () {
       $('#failure_update_voucher').modal('hide');
-    }, 2000);
+    }, 3000);
   };
   if (window.location.hash === '#failure_update_food') {
     $('#failure_update_food').modal('show');
     setTimeout(function () {
       $('#failure_update_food').modal('hide');
-    }, 2000);
+    }, 3000);
   };
   if (window.location.hash === '#failure_delete_voucher') {
     $('#failure_delete_voucher').modal('show');
     setTimeout(function () {
       $('#failure_delete_voucher').modal('hide');
-    }, 2000);
+    }, 3000);
   };
 });
