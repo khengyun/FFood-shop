c85051c44ea1864572c3314c4ade4978ef3b6119
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-11-02 14:51:17 +0700

    update  payment method on pserver for app.py

--- api/psqlserver/app.py
+++ api/psqlserver/app.py
@@ -1,8 +1,13 @@
 # app.py
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, Query
 from model.Food import FoodModel, FoodOperations
+from model.Payment import Payment, PaymentRequest, Settings
 from model.Google_Auth import GoogleAuth
+from model.Order import OrderOperations
+from model.vn_payment import Vnpay
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import uvicorn
 app = FastAPI()
 app.add_middleware(
@@ -13,7 +18,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount("/model", StaticFiles(directory="model"), name="model")
+
+
 food_operations = FoodOperations()
+order_operations = OrderOperations()
 
 
 @app.get('/get_all_food', response_model=list[FoodModel])
@@ -41,6 +50,145 @@ def get_daily_sales():
 async def get_credential(token_info: str):
     google_auth = GoogleAuth()
     return google_auth.verify_google_id_token(token_info)
+
+
+
+# payment method on pserver
+@app.get('/payment', response_model=dict)
+async def payment_endpoint(
+    request: Request,
+    vnp_OrderType: str = Query(..., alias='vnp_OrderType'),
+    vnp_OrderInfo: str = Query(..., alias='vnp_OrderInfo'),
+    vnp_Amount: int = Query(..., alias='vnp_Amount')
+):
+    payment_instance = Payment(Settings)
+    return payment_instance(PaymentRequest(vnp_OrderType=vnp_OrderType, vnp_Amount=vnp_Amount, vnp_OrderInfo=vnp_OrderInfo), request)
+
+@app.get('/payment_from_cis')
+def payment_from_cis(request: Request, cis: str = Query(..., alias='cis')):
+    try:
+        order_data = order_operations.payment_from_cis(int(cis))
+        if order_data:
+            first_order = order_data[0]
+            payment_instance = Payment(Settings)
+            return payment_instance(PaymentRequest(
+                vnp_OrderType="online_order",
+                vnp_Amount=first_order["order_total"]*100,
+                vnp_OrderInfo=str({
+                    "order_id": first_order["order_id"],
+                    "customer_id": first_order["customer_id"],
+                    "order_note": first_order["order_note"],
+                    "order_time": first_order["order_time"]
+                }),
+            ), request)
+        else:
+            return "something went wrong"
+
+    except Exception as e:
+        error_message = {"error": str(e)}
+        return [error_message]
+
+@app.get('/payment_return',response_class=HTMLResponse)
+async def payment_return(request: Request):
+    # Access and show the query parameters
+    
+    vnp_Amount = request.query_params.get('vnp_Amount')
+    vnp_BankCode = request.query_params.get('vnp_BankCode')
+    vnp_CardType = request.query_params.get('vnp_CardType')
+    vnp_OrderInfo = request.query_params.get('vnp_OrderInfo')
+    vnp_PayDate = request.query_params.get('vnp_PayDate')
+    vnp_ResponseCode = request.query_params.get('vnp_ResponseCode')
+    vnp_TmnCode = request.query_params.get('vnp_TmnCode')
+    vnp_TransactionNo = request.query_params.get('vnp_TransactionNo')
+    vnp_TransactionStatus = request.query_params.get('vnp_TransactionStatus')
+    vnp_TxnRef = request.query_params.get('vnp_TxnRef')
+    vnp_SecureHash = request.query_params.get('vnp_SecureHash')
+    
+    
+    
+    vnpay = Vnpay(Settings)
+    vnpay.responseData = {
+    "vnp_Amount": vnp_Amount,
+    "vnp_BankCode": vnp_BankCode,
+    "vnp_CardType": vnp_CardType,
+    "vnp_OrderInfo": vnp_OrderInfo,
+    "vnp_PayDate": vnp_PayDate,
+    "vnp_ResponseCode": vnp_ResponseCode,
+    "vnp_TmnCode": vnp_TmnCode,
+    "vnp_TransactionNo": vnp_TransactionNo,
+    "vnp_TransactionStatus": vnp_TransactionStatus,
+    "vnp_TxnRef": vnp_TxnRef,
+    "vnp_SecureHash": vnp_SecureHash
+    }
+    print("==============Validate response===============")
+    print({
+    "vnp_Amount": vnp_Amount,
+    "vnp_BankCode": vnp_BankCode,
+    "vnp_CardType": vnp_CardType,
+    "vnp_OrderInfo": vnp_OrderInfo,
+    "vnp_PayDate": vnp_PayDate,
+    "vnp_ResponseCode": vnp_ResponseCode,
+    "vnp_TmnCode": vnp_TmnCode,
+    "vnp_TransactionNo": vnp_TransactionNo,
+    "vnp_TransactionStatus": vnp_TransactionStatus,
+    "vnp_TxnRef": vnp_TxnRef,
+    "vnp_SecureHash": vnp_SecureHash
+    })
+    
+    if (vnp_TransactionStatus == '00'):
+         # Gọi hàm insert_payment ở đây
+        try:
+            # Trích xuất order_id từ vnp_OrderInfo
+            vnp_OrderInfo_dict = eval(vnp_OrderInfo)
+            order_id = vnp_OrderInfo_dict.get('order_id')
+            
+            # Gọi hàm insert_payment
+            result = order_operations.insert_payment(order_id, vnp_Amount, vnp_TxnRef, vnp_BankCode)
+            # Thực hiện xử lý kết quả (nếu cần) sau khi insert_payment
+            
+        except Exception as e:
+            result = str(e)
+    else:
+        # Do something when the payment is failed
+        pass
+
+    # Generate or load the HTML content
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Payment Return Page</title>
+    </head>
+    <body>
+        <h1>Payment Return Page</h1>
+        <p>Your payment has been processed successfully.</p>
+        <p>vnp_Amount: {vnp_Amount}</p>
+        <p>vnp_BankCode: {vnp_BankCode}</p>
+        <p>vnp_CardType: {vnp_CardType}</p>
+        <p>vnp_OrderInfo: {vnp_OrderInfo}</p>
+        <p>vnp_PayDate: {vnp_PayDate}</p>
+        <p>vnp_ResponseCode: {vnp_ResponseCode}</p>
+        <p>vnp_TmnCode: {vnp_TmnCode}</p>
+        <p>vnp_TransactionNo: {vnp_TransactionNo}</p>
+        <p>vnp_TransactionStatus: {vnp_TransactionStatus}</p>
+        <p>vnp_TxnRef: {vnp_TxnRef}</p>
+        <p>vnp_SecureHash: {vnp_SecureHash}</p>
+        <p>order_id: {order_id}</p>
+        <P>result: {result}</p>
+        <script>
+            // Execute the script after the page has fully loaded
+            window.onload = function() {{
+                window.location.assign("http://localhost:8080/home#success");
+            }};
+        </script>
+        <!-- Add your HTML content here -->
+    </body>
+    </html>
+    """
+    
+    return html_content
+
+
 
 
 if __name__ == '__main__': 
