218298bd3f5783134352e626088020c201337b1b
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-11-02 23:20:47 +0700

    remove print comment and add tag  at app.py

--- api/psqlserver/app.py
+++ api/psqlserver/app.py
@@ -103,7 +103,7 @@ async def payment_return(request: Request):
     vnp_TransactionStatus = request.query_params.get('vnp_TransactionStatus')
     vnp_TxnRef = request.query_params.get('vnp_TxnRef')
     vnp_SecureHash = request.query_params.get('vnp_SecureHash')
-    
+    tag = "#failure"
     
     
     vnpay = Vnpay(Settings)
@@ -121,19 +121,6 @@ async def payment_return(request: Request):
     "vnp_SecureHash": vnp_SecureHash
     }
     print("==============Validate response===============")
-    print({
-    "vnp_Amount": vnp_Amount,
-    "vnp_BankCode": vnp_BankCode,
-    "vnp_CardType": vnp_CardType,
-    "vnp_OrderInfo": vnp_OrderInfo,
-    "vnp_PayDate": vnp_PayDate,
-    "vnp_ResponseCode": vnp_ResponseCode,
-    "vnp_TmnCode": vnp_TmnCode,
-    "vnp_TransactionNo": vnp_TransactionNo,
-    "vnp_TransactionStatus": vnp_TransactionStatus,
-    "vnp_TxnRef": vnp_TxnRef,
-    "vnp_SecureHash": vnp_SecureHash
-    })
     
     if (vnp_TransactionStatus == '00'):
          # Gọi hàm insert_payment ở đây
@@ -145,6 +132,7 @@ async def payment_return(request: Request):
             # Gọi hàm insert_payment
             result = order_operations.insert_payment(order_id, vnp_Amount, vnp_TxnRef, vnp_BankCode)
             # Thực hiện xử lý kết quả (nếu cần) sau khi insert_payment
+            tag = "#success"
 
             
         except Exception as e:
@@ -179,7 +167,7 @@ async def payment_return(request: Request):
         <script>
             // Execute the script after the page has fully loaded
             window.onload = function() {{
-                window.location.assign("http://localhost:8080/home#success");
+                window.location.assign("http://localhost:8080/home{tag}");
             }};
         </script>
         <!-- Add your HTML content here -->
